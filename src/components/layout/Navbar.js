f557972a335e600f15589610eae628bb02f84e9c
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-01-11 10:26:11 -0300

    add estilos

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,27 +1,30 @@
 import { Container } from "./Container";
 import { Link } from "react-router-dom";
-// import logo from "../../assets/images/costs_logo.png";
+import logo from "../../assets/images/costs_logo.png";
+import styles from "./Navbar.module.css";
 
 export function Navbar() {
   return (
-    <div>
+    <nav className={styles.navbar}>
       <Container>
-        <Link to="/">{/* <img src={logo} alt="Costs" /> */}</Link>
-        <ul>
-          <li>
+        <Link to="/">
+          <img src={logo} alt="Costs" />
+        </Link>
+        <ul className={styles.list}>
+          <li className={styles.item}>
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className={styles.item}>
             <Link to="/projects">Projetos</Link>
           </li>
-          <li>
+          <li className={styles.item}>
             <Link to="/company">Empresa</Link>
           </li>
-          <li>
+          <li className={styles.item}>
             <Link to="/contact">Contato</Link>
           </li>
         </ul>
       </Container>
-    </div>
+    </nav>
   );
 }
