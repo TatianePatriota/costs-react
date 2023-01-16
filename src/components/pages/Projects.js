8482e9becd63fb599833e6b645a8606e76a6c411
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-01-16 16:14:45 -0300

    add condicao para mensagem de erro

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,3 +1,17 @@
+import { Message } from "../layout/Message";
+import { useLocation } from "react-router-dom";
+
 export function Projects() {
-  return <h1>Projects</h1>;
+  const location = useLocation();
+  let message = "";
+  if (location.state) {
+    message = location.state.message;
+  }
+
+  return (
+    <div>
+      <h1>Meus Projetos</h1>
+      {message && <Message type="success" msg={message} />}
+    </div>
+  );
 }
