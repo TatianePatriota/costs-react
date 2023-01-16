a948fa083b5eac4c35a4f3f35ccbbdf262f71104
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-01-16 06:51:30 -0300

    add fecth

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,12 +1,32 @@
 import { ProjectForm } from "../project/ProjectForm";
 import styles from "./NewProject.module.css";
+import { useNavigate } from "react-router-dom";
 
 export function NewProject() {
+  const history = useNavigate();
+
+  function createPost(project) {
+    project.cost = 0;
+    project.services = [];
+
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        history("/projects", { message: "Projeto criado com sucesso!" });
+      });
+  }
+
   return (
     <div className={styles.newproject_container}>
       <h1>Criar Projeto</h1>
-      <p>Crie seu projeto para depois adicionar os serviços.</p>
-      <ProjectForm btnText="Criar Projeto" />
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
     </div>
   );
 }
