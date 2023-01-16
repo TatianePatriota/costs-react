b9c37ae50b9bce48886d31574602ba8aa8b693d1
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-01-16 06:51:15 -0300

    add funcoes

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -4,8 +4,9 @@ import { SubmitButton } from "../form/SubmitButton";
 import styles from "./ProjectForm.module.css";
 import { useEffect, useState } from "react";
 
-export function ProjectForm({ btnText }) {
+export function ProjectForm({ handleSubmit, btnText, projectData }) {
   const [categories, setCategories] = useState([]);
+  const [project, setProject] = useState(projectData || {});
 
   useEffect(() => {
     fetch("  http://localhost:5000/categories", {
@@ -21,24 +22,49 @@ export function ProjectForm({ btnText }) {
       .catch((err) => console.log(err));
   }, []);
 
+  const submit = (e) => {
+    e.preventDefault();
+    handleSubmit(project);
+  };
+
+  function handleChange(e) {
+    setProject({ ...project, [e.target.name]: e.target.value });
+  }
+
+  function handleCategory(e) {
+    setProject({
+      ...project,
+      category: {
+        id: e.target.value,
+        name: e.target.options[e.target.selectedIndex].text,
+      },
+    });
+  }
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={submit} className={styles.form}>
       <Input
         type="text"
         text="Nome do Projeto"
         name="name"
         placeholder="Insira o nome do projeto"
+        handleOnChange={handleChange}
+        value={project.name ? project.name : ""}
       />
       <Input
         type="number"
         text="Orçamento do projeto"
         name="budget"
         placeholder="Insira o orçamento total"
+        handleOnChange={handleChange}
+        value={project.budget ? project.budget : ""}
       />
       <Select
         text="Selecione a categoria"
         name="category_id"
         options={categories}
+        handleOnChange={handleCategory}
+        value={project.category ? project.category.id : ""}
       />
       <SubmitButton text={btnText} />
     </form>
