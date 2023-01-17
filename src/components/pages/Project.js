20c673e9a48875b894d208f9b647e34c9a47e3c6
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-01-17 14:29:33 -0300

    add parte de serviços e incluindo states

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -3,11 +3,16 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { Loading } from "../layout/Loading";
 import { Container } from "../layout/Container";
+import { ProjectForm } from "../project/ProjectForm";
+import { Message } from "../layout/Message";
 
 export function Project() {
   const { id } = useParams();
   const [project, setProject] = useState([]);
   const [showProjectForm, setShowProjectForm] = useState(false);
+  const [showServiceForm, setShowServiceForm] = useState(false);
+  const [message, setMessage] = useState();
+  const [type, setType] = useState();
 
   useEffect(() => {
     setTimeout(() => {
@@ -25,8 +30,36 @@ export function Project() {
     }, 200);
   }, [id]);
 
+  function editPost(project) {
+    setMessage("");
+    if (!project.budget < project.cost) {
+      setMessage("O orçamento não pode ser mmenor que o custo do projeto!");
+      setType("error");
+      return false;
+    }
+    fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProject(data);
+        setShowProjectForm(false);
+        setMessage("Projeto atualizado!");
+        setType("success");
+      })
+      .catch((err) => console.log(err));
+  }
+
   function toggleProjectForm() {
     setShowProjectForm(!showProjectForm);
+  }
+
+  function toggleServiceForm() {
+    setShowServiceForm(!showServiceForm);
   }
 
   return (
@@ -34,6 +67,7 @@ export function Project() {
       {project.name ? (
         <div className={styles.project_details}>
           <Container customClass="column">
+            {message && <Message type={type} msg={message} />}
             <div className={styles.details_container}>
               <h1>Projeto: {project.name}</h1>
               <button className={styles.btn} onClick={toggleProjectForm}>
@@ -56,10 +90,27 @@ export function Project() {
                 </div>
               ) : (
                 <div className={styles.project_info}>
-                  <p>Detalhes do projeto</p>
+                  <ProjectForm
+                    handleSubmit={editPost}
+                    btnText="Concluir Edição"
+                    projectData={project}
+                  />
                 </div>
               )}
             </div>
+            <div className={styles.service_form_container}>
+              <h2>Adicione um serviço:</h2>
+              <button className={styles.btn} onClick={toggleServiceForm}>
+                {!showServiceForm ? "Adicionar serviço" : "Fechar"}
+              </button>
+              <div className={styles.project_info}>
+                {showServiceForm && <div>Formulário do serviço</div>}
+              </div>
+            </div>
+            <h2>Serviços</h2>
+            <Container customClass="start">
+              <p>Itens de Serviços</p>
+            </Container>
           </Container>
         </div>
       ) : (
