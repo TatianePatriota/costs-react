d193250d82866ba76d72a08c15981aa8b142144e
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-01-18 08:27:11 -0300

    add contatos

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,3 +1,9 @@
 export function Contact() {
-  return <h1>Contact</h1>;
+  return (
+    <div>
+      <h1>Contatos:</h1>
+      <p>Celular: 22 9888-8886</p>
+      <p>Rua Neide São Brito, Araruama</p>
+    </div>
+  );
 }
